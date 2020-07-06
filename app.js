f2151eff2db44dfcf8014933a9fae2c880559449
ceifa <gabrinelson27@gmail.com>
2020-07-06 13:56:59 -0300

    Add environment variable

--- app.js
+++ app.js
@@ -6,4 +6,5 @@ const jungleConfig = require('./jungle.config');
 
 const app = express();
 
-startGraphqlServer(jungleConfig, __dirname, () => readRoutes(jungleConfig, app, __dirname).then(() => stopGraphqlServer(() => startAppServer(app))));
+startGraphqlServer(jungleConfig, __dirname, () => readRoutes(jungleConfig, app, __dirname)
+    .then(() => stopGraphqlServer(() => process.env.PRODUCTION ? null : startAppServer(app))));
--- build.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const { startGraphqlServer, stopGraphqlServer, readRoutes } = require('junglejs');
-
-const jungleConfig = require('./jungle.config');
-
-const app = express();
-
-startGraphqlServer(jungleConfig, __dirname, () => readRoutes(jungleConfig, app, __dirname).then(() => stopGraphqlServer(() => null)));
